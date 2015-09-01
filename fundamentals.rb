3f9ff8c0b657f41eea2201ee33b3827c853872e1
michelfrechette <mifrec@hotmail.com>
2015-08-31 22:49:55 -0400

    Control Structures

---
+++ fundamentals.rb
@@ -0,0 +1,124 @@
+# Control Structures
+
+# if statements
+
+# my_value = 5
+
+# if my_value > 4
+#   puts "I get printed"
+# end
+
+# else
+
+# number = 5
+
+# if number > 0
+#   puts "#{number} is positive"
+# else
+#   puts "#{number} is negative"
+# end
+
+# elsif
+
+# number1 = 2
+# number2 = 5
+
+# if number1 > number2
+#   puts "number1 is greater than number2"
+# elsif number1 < number2
+#   puts "number1 is lesser than number2"
+# else
+#   puts "number1 is equal to number2"
+# end
+
+# unless
+
+# if number != 10
+#   puts "I get printed"
+# end
+
+# unless number == 10
+#   puts "Not 10!"
+# else
+#   puts "It's 10"
+# end
+
+# if number == 10
+#   puts "It's 10"
+# else
+#   puts "Not 10"
+# end
+
+
+# if number == 10
+#   puts"It's 10"
+# end
+
+# inline if /unless
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Loops
+# Repeat
+
+# puts "Hello"
+# puts "Hello"
+# puts "Hello"
+# puts "Hello"
+# puts "Hello"
+# puts "Hello"
+
+# while true
+#   puts "Hello"
+# end
+
+
+# counter = 1
+
+# while counter < 4
+#   puts "Counter at #{counter}"
+#   counter -= 1
+# end
+
+
+# counter = 5
+# until counter == 0
+#   puts "Counter at #{counter}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   puts number
+# end
+
+
+# range
+
+# one_to_ten = (1..10)
+
+# one_to_ten.each do |number|
+#   puts number
+# end
